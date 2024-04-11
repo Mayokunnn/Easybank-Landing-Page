dc8ea81be6c7adc3e46f5cbf8273f44c6ed285fe
Kelvin Mayokun Areola <kareola960@gmail.com>
2024-04-11 19:20:46 +0100

    Adding animations and preloader effects

--- script.js
+++ script.js
@@ -45,3 +45,36 @@ window.addEventListener('resize', (e) => {
   mainMenu.classList.remove('active');
   menuButton.classList.remove('is-active');
 });
+
+let tl = gsap.timeline({ defaults: { ease: 'power4.inOut', duration: 1, delay: .4 } });
+
+tl.to('.nav__logo', {
+  clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+  opacity: 1,
+  y: 0,
+})
+  .to(
+    mainMenu.children,
+    { opacity: 1, y: 0, stagger: 0.4, duration: 0.4 },
+    '-=0.5'
+  )
+  .from(header.children, { y: '100vh', stagger: 0.4, opacity: 0 }, '-=1')
+
+  const myText = new SplitType('#text')
+
+  gsap.to('.char', {
+    y: 0,
+    duration: 1,
+    stagger: .1,
+    ease: 'bounce',
+    opacity: 1,
+    clipPath: 'polygon(0 100%, 100% 100%, 100% 0, 0 0)',
+    delay: 0.3,
+    onComplete: function () {
+      // Hide the preloader and show the main content
+      setTimeout(function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('main').style.display = 'block';
+      }, 500);
+    },
+  });
